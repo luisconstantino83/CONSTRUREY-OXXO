cb340d1d42164ad220fa72acf3eef36d82380b4c
luisconstantino83 <luis_ouster08@hotmail.com>
2026-05-14 13:20:36 -0500

    Add TypeScript and ESLint build error ignore options

--- next.config.js
+++ next.config.js
@@ -1,5 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  typescript: { ignoreBuildErrors: true },
+  eslint: { ignoreDuringBuilds: true },
   experimental: { serverActions: { allowedOrigins: ['*'] } },
   images: { remotePatterns: [{ protocol: 'https', hostname: '**' }] },
 }
